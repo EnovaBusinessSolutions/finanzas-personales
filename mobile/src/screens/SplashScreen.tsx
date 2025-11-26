123c1fbf5d535820ced5cffd5c3309057bf053be
Jose Manuel Mejia <josemanuelmejiadominguez2@gmail.com>
2025-11-26 01:32:53 -0600

    w

--- mobile/src/screens/SplashScreen.tsx
+++ mobile/src/screens/SplashScreen.tsx
@@ -70,7 +70,7 @@ const styles = StyleSheet.create({
   fromLabel: {
     fontSize: 18,          // texto “from” grande
     color: COLORS.muted,
-    marginBottom: -13,      // ⬅️ casi pegado al logo E-nova
+    marginBottom: -20,      // ⬅️ casi pegado al logo E-nova
     fontWeight: '500',
   },
   enovaLogo: {
